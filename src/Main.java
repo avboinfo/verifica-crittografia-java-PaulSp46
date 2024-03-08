1b42c3742227004bad7dc502d4d10eedcae2d675
PaulSp46 <136258721+PaulSp46@users.noreply.github.com>
2024-03-08 07:44:10 +0000

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final String[] menu = {"Cripta il file", "Decripta il file", "Stampa il file (solo se decriptato)"};
+        Scanner keyInput = new Scanner(System.in);
+        int choice = Utility.scegli(menu, keyInput);
     }
 }
